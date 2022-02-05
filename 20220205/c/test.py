b66d25f8285f1666ca9246b2eaabef928f89bd16
seigot <s.takada.3o3@gmail.com>
2022-02-06 00:14:50 +0900

    update

--- 20220205/c/test.py
+++ 20220205/c/test.py
@@ -3,15 +3,18 @@ res=0
 t=9
 mod=998244353
 
-while n>t:
+KETA=len(str(n))
+
+#while n>t:
+for i in range(KETA-1):
     # 対象の桁の合計値を求める
     res+=(t*(t+1))//2
     res%=mod
-    # 対象の桁の合計値は求めたので差分
-    n-=t
     # 次の桁
     t*=10
+
 # 一番上の桁の合計値を求める
+n = n - ((10**(KETA-1))-1)
 res+=(n*(n+1))//2
 
 res%=mod
