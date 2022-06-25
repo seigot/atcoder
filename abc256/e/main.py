e924de92b25254bc0c2450935f84ea05864733d8
seigot <s.takada.3o3@gmail.com>
2022-06-26 01:19:46 +0900

    update

--- abc256/e/main.py
+++ abc256/e/main.py
@@ -10,3 +10,38 @@ INF = float("inf")
 MINF = -float("inf")
 
 # A,B=map(int, input().split())      # (2)数字が2つ以上で別々に受け取り  入力例:A B
+
+N = int(input())
+X = [-1] + list(map(int, input().split()))
+C = [-1] + list(map(int, input().split()))
+
+visited = [False] * (N+1)
+tmp_visited = [False] * (N+1)
+ans = 0
+
+# ノードiiに着目して深さ優先探索する
+def check(ii):
+    global ans
+    if visited[ii] == True:
+        return
+    if tmp_visited[ii] == True:
+        # ループ検出
+        # 次のノードから探索する
+        tx = X[ii]
+        cost = C[ii]
+        while tx != ii:
+            # ループ中の最小コスト検出
+            tx = X[tx]
+            cost = min(cost, C[tx])
+        # 最小コストは除いてもOK
+        ans += cost
+        return 
+    tmp_visited[ii] = True
+    check(X[ii])
+    # 訪れたノードは全て印をつける
+    visited[ii] = True
+
+for ii in range(1, N+1):
+    check(ii)
+
+print(ans)
