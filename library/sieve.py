79bf9146bf33832b4b5e96c8f87302c64929daf7
seigot <s.takada.3o3@gmail.com>
2023-05-02 19:02:10 +0900

    add sieve function

---
+++ library/sieve.py
@@ -0,0 +1,15 @@
+def sieve(L,R):
+    if L<2: L=2
+    flag=[0]*(R+1)
+    for i in range(2, min(R+1,int(R**.5)+5)):
+        if flag[i]:continue
+        for j in range(i**2,R+1, i):
+            if flag[j]:continue
+            if j % i == 0: flag[j] = 1
+    # return [i for i in range(L,R+1) if flag[i] == 0], flag #flagをèしたæがäåなåå
+    return [i for i in range(L,R+1) if flag[i] == 0]
+  
+# L~Rまでの素数を列挙する(ex.2~3*(10**5))
+# 場合の数を求める時は大きな数を早々に探索打ち切ると高速である場合が多い
+P = sieve(2,3*(10**5)) 
+
