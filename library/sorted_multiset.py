b0d538d6f2dd52a338ddf4d8771352c1808107e5
seigot <s.takada.3o3@gmail.com>
2023-06-18 00:43:40 +0900

    update

--- library/sorted_multiset.py
+++ library/sorted_multiset.py
@@ -1,41 +1,45 @@
-# https://github.com/tatyam-prime/SortedSet/blob/main/SortedSet.py
-import math
-from bisect import bisect_left, bisect_right
-from typing import Generic, Iterable, Iterator, TypeVar, Optional, List
-T = TypeVar('T')
-
-class SortedSet(Generic[T]):
+# https://github.com/tatyam-prime/SortedSet/blob/main/SortedMultiset.py
+from bisect import insort
+from typing import Generic, Iterable, Iterator, TypeVar, Union, List
+T = TypeVar("T")
+class SortedMultiset(Generic[T]):
     BUCKET_RATIO = 50
     REBUILD_RATIO = 170
 
     def _build(self, a=None) -> None:
         "Evenly divide `a` into buckets."
-        if a is None: a = list(self)
+        if a is None:
+            a = list(self)
         size = self.size = len(a)
         bucket_size = int(math.ceil(math.sqrt(size / self.BUCKET_RATIO)))
-        self.a = [a[size * i // bucket_size : size * (i + 1) // bucket_size] for i in range(bucket_size)]
-    
+        self.a = [
+            a[size * i // bucket_size : size * (i + 1) // bucket_size]
+            for i in range(bucket_size)
+        ]
+
     def __init__(self, a: Iterable[T] = []) -> None:
-        "Make a new SortedSet from iterable. / O(N) if sorted and unique / O(N log N)"
+        "Make a new SortedMultiset from iterable. / O(N) if sorted / O(N log N)"
         a = list(a)
-        if not all(a[i] < a[i + 1] for i in range(len(a) - 1)):
-            a = sorted(set(a))
+        if not all(a[i] <= a[i + 1] for i in range(len(a) - 1)):
+            a = sorted(a)
         self._build(a)
 
     def __iter__(self) -> Iterator[T]:
         for i in self.a:
-            for j in i: yield j
+            for j in i:
+                yield j
 
     def __reversed__(self) -> Iterator[T]:
         for i in reversed(self.a):
-            for j in reversed(i): yield j
-    
+            for j in reversed(i):
+                yield j
+
     def __len__(self) -> int:
         return self.size
-    
+
     def __repr__(self) -> str:
-        return "SortedSet" + str(self.a)
-    
+        return "SortedMultiset" + str(self.a)
+
     def __str__(self) -> str:
         s = str(list(self))
         return "{" + s[1 : len(s) - 1] + "}"
@@ -43,74 +47,83 @@ class SortedSet(Generic[T]):
     def _find_bucket(self, x: T) -> List[T]:
         "Find the bucket which should contain x. self must not be empty."
         for a in self.a:
-            if x <= a[-1]: return a
+            if x <= a[-1]:
+                return a
         return a
 
     def __contains__(self, x: T) -> bool:
-        if self.size == 0: return False
+        if self.size == 0:
+            return False
         a = self._find_bucket(x)
         i = bisect_left(a, x)
         return i != len(a) and a[i] == x
 
-    def add(self, x: T) -> bool:
-        "Add an element and return True if added. / O(√N)"
+    def count(self, x: T) -> int:
+        "Count the number of x."
+        return self.index_right(x) - self.index(x)
+
+    def add(self, x: T) -> None:
+        "Add an element. / O(√N)"
         if self.size == 0:
             self.a = [[x]]
             self.size = 1
-            return True
+            return
         a = self._find_bucket(x)
-        i = bisect_left(a, x)
-        if i != len(a) and a[i] == x: return False
-        a.insert(i, x)
+        insort(a, x)
         self.size += 1
         if len(a) > len(self.a) * self.REBUILD_RATIO:
             self._build()
-        return True
 
     def discard(self, x: T) -> bool:
         "Remove an element and return True if removed. / O(√N)"
-        if self.size == 0: return False
+        if self.size == 0:
+            return False
         a = self._find_bucket(x)
         i = bisect_left(a, x)
-        if i == len(a) or a[i] != x: return False
+        if i == len(a) or a[i] != x:
+            return False
         a.pop(i)
         self.size -= 1
-        if len(a) == 0: self._build()
+        if len(a) == 0:
+            self._build()
         return True
-    
-    def lt(self, x: T) -> Optional[T]:
+
+    def lt(self, x: T) -> Union[T, None]:
         "Find the largest element < x, or None if it doesn't exist."
         for a in reversed(self.a):
             if a[0] < x:
                 return a[bisect_left(a, x) - 1]
 
-    def le(self, x: T) -> Optional[T]:
+    def le(self, x: T) -> Union[T, None]:
         "Find the largest element <= x, or None if it doesn't exist."
         for a in reversed(self.a):
             if a[0] <= x:
                 return a[bisect_right(a, x) - 1]
 
-    def gt(self, x: T) -> Optional[T]:
+    def gt(self, x: T) -> Union[T, None]:
         "Find the smallest element > x, or None if it doesn't exist."
         for a in self.a:
             if a[-1] > x:
                 return a[bisect_right(a, x)]
 
-    def ge(self, x: T) -> Optional[T]:
+    def ge(self, x: T) -> Union[T, None]:
         "Find the smallest element >= x, or None if it doesn't exist."
         for a in self.a:
             if a[-1] >= x:
                 return a[bisect_left(a, x)]
-    
+
     def __getitem__(self, x: int) -> T:
         "Return the x-th element, or IndexError if it doesn't exist."
-        if x < 0: x += self.size
-        if x < 0: raise IndexError
+        if x < 0:
+            x += self.size
+        if x < 0:
+            raise IndexError
         for a in self.a:
-            if x < len(a): return a[x]
+            if x < len(a):
+                return a[x]
             x -= len(a)
         raise IndexError
-    
+
     def index(self, x: T) -> int:
         "Count the number of elements < x."
         ans = 0
@@ -129,8 +142,10 @@ class SortedSet(Generic[T]):
             ans += len(a)
         return ans
 
-ss = SortedSet([]) # 集合
-ss.add(val)       # valを追加
-ss.discard(val)   # valを削除
-ss.gt(0)          # valより大きい最小の値を返す
+
+A = list(map(int,input().split()))
+sms = SortedMultiset(A)
+sms.add(y)        # 値を1つ追加
+sms.discard(A[x]) # 値を1つ破棄
+v = sms[-K]       # 降順K番目の値
 
