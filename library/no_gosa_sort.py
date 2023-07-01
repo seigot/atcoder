5ddf82cd27d842b0eacd9d31aeb1bff24f5db3e3
seigot <s.takada.3o3@gmail.com>
2023-07-02 01:01:55 +0900

    update

--- library/no_gosa_sort.py
+++ library/no_gosa_sort.py
@@ -8,6 +8,8 @@ def cmp(a, b):  # a = [x_a, y_a], b = [x_b / y_b]
         return 0
     # a, bという順で並んでほしい条件のときは-1を返し、それ以外では1を返す
     return -1 if a[0] * b[1] < b[0] * a[1] else 1
+    # 降順にしたい場合は、不等号をかえる
+    #return -1 if a[0] * b[1] > b[0] * a[1] else 1
 
 l = [[5, 7], [3, 8], [1, 2]]  # リストの各要素は [分子, 分母] とする
 l = sorted(l, key=cmp_to_key(cmp))
