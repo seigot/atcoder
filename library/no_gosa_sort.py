a7b521c8b0987a58e308f3ce2282235cc2fb7704
seigot <s.takada.3o3@gmail.com>
2023-07-02 10:22:18 +0900

    update

--- library/no_gosa_sort.py
+++ library/no_gosa_sort.py
@@ -13,4 +13,23 @@ def cmp(a, b):  # a = [x_a, y_a], b = [x_b / y_b]
 
 l = [[5, 7], [3, 8], [1, 2]]  # リストの各要素は [分子, 分母] とする
 l = sorted(l, key=cmp_to_key(cmp))
-# l = [[3, 8], [1, 2], [5, 7]]
+print(l) # l = [[3, 8], [1, 2], [5, 7]]
+
+from functools import cmp_to_key
+def cmp(a, b):  # a = [x_a, y_a], b = [x_b / y_b]
+    # 比較対象の分数 a と b が等しければ 0 を返す
+    if a[0] * b[1] == b[0] * a[1]:
+        ## [2]にidがある場合
+        if len(a) == 3:
+            if a[2] < b[2]:
+                # 同じa,bにindexがある場合indexの小さい順に並び替える 
+                return -1
+        return 0
+    # a, bという順で並んでほしい条件のときは-1を返し、それ以外では1を返す
+    return -1 if a[0] * b[1] > b[0] * a[1] else 1
+l = [[5, 7], [3, 8], [3, 8], [1, 2]]  # リストの各要素は [分子, 分母] とする
+l = sorted(l, key=cmp_to_key(cmp))
+print(l) # [5, 7], [1, 2], [3, 8], [3, 8]]
+l = [[5, 7, 1], [3, 8, 3], [3, 8, 2], [1, 2, 4]]  # リストの各要素は [分子, 分母] とする
+l = sorted(l, key=cmp_to_key(cmp))
+print(l) # [[5, 7, 1], [1, 2, 4], [3, 8, 2], [3, 8, 3]]
