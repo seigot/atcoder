56837a2db474b19a2a3f673f902bc98847f630b2
seigot <s.takada.3o3@gmail.com>
2023-07-23 17:46:43 +0900

    update

--- library/bfs.py
+++ library/bfs.py
@@ -71,3 +71,53 @@ def bfs(s, n):
     return depth, pre
 
 d, _ = bfs(1, n+m+1)
+
+# 幅優先探索（方向に状態をもつ）
+dy = [1,-1,0,0]
+dx = [0,0,1,-1]
+def bfs(s, st):
+    que = deque([(s,st)])
+    # 状態を持ったまま幅優先探索する
+    # 上下左右通過した方向に状態を持たせる
+    depth = [[-1]*5 for i in range(n*m)]
+    depth[s][st] = 0
+    while que:
+        crr,state = que.popleft()
+        x,y = crr//m, crr%m
+        if state == 4:
+            # 4方向に移動できる状態
+            for i in range(4):
+                nx,ny = x + dx[i], y + dy[i]
+                nxt = nx*m+ny
+                if (nx < 0 or n <= nx or ny < 0 or m <= ny):
+                    # エリア外であればskip
+                    continue
+                if S[nx][ny] != "#" and depth[nxt][i] == -1:
+                    # 岩ではなく未探索である場合は探索実施
+                    depth[nxt][i] = 1#depth[crr][state]+1
+                    que.append((nxt, i))
+        else:
+            # 1方向にのみ移動できる状態
+            # state=0~3 (dx,dyに対応する)
+            nx,ny = x + dx[state], y + dy[state]
+            nxt = nx*m+ny
+            if (nx < 0 or n <= nx or ny < 0 or m <= ny):
+                # エリア外であればskip
+                continue
+            if S[nx][ny] == "#": #and depth[crr][4] == -1:
+                # 岩の場合は4方向に探索
+                depth[crr][4] = 1#depth[crr][state] + 1
+                que.append((crr, 4))
+            elif S[nx][ny] != "#" and depth[nxt][state] == -1:
+                # 岩ではなく未探索である場合は探索実施
+                depth[nxt][state] = 1#depth[crr][state] + 1
+                que.append((nxt, state))
+    return depth
+
+d = bfs(m+1,4)
+ans = 0
+for i in range(n):
+    for j in range(m):
+        # 一度でも通過した事がある場合は+1
+        ans += sum(d[i*m+j]) != -5
+print(ans)
