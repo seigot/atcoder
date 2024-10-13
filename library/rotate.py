1d6049153b6f2e3febdfc093684c05788ee8af2f
seigot <s.takada.3o3@gmail.com>
2024-10-13 02:06:34 -0700

    update

---
+++ library/rotate.py
@@ -0,0 +1,13 @@
+# rotate grid by 90 degrees 
+# 1 2    2 4 6
+# 3 4 -> 1 3 5
+# 5 6 
+def rot(grid):
+    h,w = n,n
+    new = [['']*h for i in range(w)]
+    for i in range(w):
+        for j in range(h):
+            new[i][j] = grid[j][w-1-i]
+    h, w = w, h
+    return new
+
